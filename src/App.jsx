17dc77817f0012c7b5dc0d4e9dfa74ff970c328e
sauce-coder-2025 <luke.gray@fisherpaykel.com>
2024-12-05 12:37:46 +1300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,16 @@
 // src/App.jsx
 import React from 'react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider, ProtectedRoute } from './auth/AuthProvider';
 import MainContent from './components/Layout/MainContent';
 
 const App = () => {
   return (
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-      <AuthProvider>
-        <ProtectedRoute>
-          <MainContent />
-        </ProtectedRoute>
-      </AuthProvider>
-    </GoogleOAuthProvider>
+    <AuthProvider>
+      <ProtectedRoute>
+        <MainContent />
+      </ProtectedRoute>
+    </AuthProvider>
   );
 };
 
 export default App;
-
-// src/index.jsx
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
